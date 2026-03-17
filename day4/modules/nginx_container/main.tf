b57fb3df9d3b368b3a181b26c08ea8b1c9409320
bhargavram8035-dot <bhargavram8035@gmail.com>
2026-03-17 16:43:18 +0000

    day4 complete

---
+++ day4/modules/nginx_container/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+variable "container_name" {}
+variable "container_port" {}
+variable "image_name" {}
+
+resource "docker_image" "nginx" {
+  name = var.image_name
+}
+
+resource "docker_container" "container" {
+  name  = var.container_name
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = var.container_port
+  }
+}
